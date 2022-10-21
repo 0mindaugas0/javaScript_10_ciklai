a4fa82ffeee2b67765c7ee3fba4204aab8025821
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-10-21 09:12:28 +0300

    pridėta trečia užduotis

---
+++ script10-3.js
@@ -0,0 +1,9 @@
+let zodis;
+zodis = prompt("Įveskite žodį");
+
+let n;
+n = +prompt("Įveskite parametrą, kiek kartų norite pakartoti žodį");
+
+for (let i = 0; i < n; i++) {
+    console.log(zodis);
+}
